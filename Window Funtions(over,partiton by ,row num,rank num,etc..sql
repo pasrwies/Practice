b80743d217833193b6f54d699ce7e7f15edf7cd5
pasrwies <phyusyn.win95@gmail.com>
2025-06-15 01:16:53 +0900

    Update Window Funtions(over,partiton by ,row num,rank num,etc..sql

--- Window Funtions(over,partiton by ,row num,rank num,etc..sql
+++ Window Funtions(over,partiton by ,row num,rank num,etc..sql
@@ -1,4 +1,4 @@
---Window Functions
+--  Window Functions
 
 
 SELECT gender, AVG(salary) AS avg_salary
@@ -8,25 +8,25 @@ JOIN employee_salary sal
 GROUP BY gender
 ;
 
---Window Functions
+--  Window Functions
 
 SELECT gender, AVG(salary) OVER()
 FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
-everyone and all avg_salary
+-- everyone and all avg_salary
 
 
---Over(Partiton By)
+--  Over(Partiton By)
 
 
 SELECT gender, AVG(salary) OVER(PARTITION BY gender)
 FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
-same gender group of avg_salary
+-- same gender group of avg_salary
 
---eg. of use window functions in two different table
+-- eg. of use window functions in two different table
 
 
 SELECT dem.first_name, dem.last_name, gender, AVG(salary) AS avg_salary
@@ -34,8 +34,8 @@ FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id
 GROUP BY dem.first_name, dem.last_name, gender;
-This one gave different result because they`ve hold difference value.
-Thsi one is calculated base on individual.
+-- This one gave different result because they`ve hold difference value.
+-- Thsi one is calculated base on individual.
 
 
 
@@ -44,11 +44,11 @@ SELECT dem.first_name, dem.last_name, gender, AVG(salary) OVER(PARTITION BY gend
 FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
-(This result of avg_salary is calculated base on gender group)
+-- (This result of avg_salary is calculated base on gender group)
 
 
 
----Rolling Total
+-- Rolling Total
 
 
 SELECT dem.first_name, dem.last_name, gender, salary,
@@ -57,21 +57,21 @@ FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
 
---Row number()
+-- Row number()
 
 SELECT dem.first_name, dem.last_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender)
 FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
-Give the row number Base On gender.
+-- Give the row number Base On gender.
 
 SELECT dem.first_name, dem.last_name, gender, salary,
 ROW_NUMBER() OVER()
 FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
-Give the row number every row different value.Not base on gender.
+-- Give the row number every row different value.Not base on gender.
 
 SELECT dem.first_name, dem.last_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC)
@@ -79,10 +79,10 @@ FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
 
-Give row number base on salary.Even salary is the same the result give different row number.
+-- Give row number base on salary.Even salary is the same the result give different row number.
 
 
---Rank 
+-- Rank 
 
 SELECT dem.first_name, dem.last_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
@@ -91,11 +91,11 @@ FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
 
-But rank is different.Cuz if the salary is the same,rank give the same number to the same salary row.
-And after that the next row numvber will become become the next value.Eg,5,5 and the next will be 7.
+-- But rank is different.Cuz if the salary is the same,rank give the same number to the same salary row.
+-- And after that the next row numvber will become become the next value.Eg,5,5 and the next will be 7.
 
 
---Dense Rank
+-- Dense Rank
 
 SELECT dem.first_name, dem.last_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
@@ -105,6 +105,6 @@ FROM employee_demographics dem
 JOIN employee_salary sal
    ON dem.employee_id = sal.employee_id;
 
-DENSE rank function is the same with rank function.But the different is the next row number will be the same.
-Eg,5,5 and the next will be 6 not 7.
+-- DENSE rank function is the same with rank function.But the different is the next row number will be the same.
+-- Eg,5,5 and the next will be 6 not 7.
 
